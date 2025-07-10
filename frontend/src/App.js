4c9bf091d04cfc52f249de87bf0a577e923484a3
Nitesh14022004 <niteshsince04@gmail.com>
2025-07-10 10:48:56 +0530

    frontend-part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
+import Authentication from './pages/authentication';
+import History from './pages/history';
+import HomeComponent from './pages/home';
+import LandingPage from './pages/landing';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+
+        <AuthProvider>
+
+
+          <Routes>
+
+            <Route path='/' element={<LandingPage />} />
+
+            <Route path='/auth' element={<Authentication />} />
+
+            <Route path='/home's element={<HomeComponent />} />
+            <Route path='/history' element={<History />} />
+            <Route path='/:url' element={<VideoMeetComponent />} />
+          </Routes>
+        </AuthProvider>
+
+      </Router>
     </div>
   );
 }
